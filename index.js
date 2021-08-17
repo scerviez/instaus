7d445a702e55c95443a3dc79d585b0f6f63614b4
nekozu <81870902+nekozu@users.noreply.github.com>
2021-08-17 13:50:14 +0700

    hm

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ client.on('connected', () => {
 });
 
 client.on('messageCreate', async function(ctx) {
-    var text = ctx.content;
-    if (RegExp(".ping", "i").exec(text)) {
-        let t0 = performance.now();
-        let t1 = performance.now();
-        let diff = ((t1 - t0) / 1000).toLocaleString('id-ID', { maximumFractionDigits: 3 });
-        let message = `Pong!\nIn <code>${diff}</code> seconds.`
-        return ctx.reply(message)
-    }
-    if (RegExp('.help',"i").exec(text)){
-        return ctx.reply('Available Command\n .ping .help .google .lirik')
-    }                                                                                                 
-    if (RegExp(".lirik", 'i').exec(text)) {                                                                               
-        var reqy = await axios.get(`https://lyrics-api.xlaaf.repl.co/search?q=${text.replace(/([.*kbbi ])/ig,"")}`)
-       var message = 'Ditemukan: '+reqy.data.data
-       return ctx.reply(message)
-    }
-    if (new RegExp(".google ", "i").exec(text)) {
+  try {
+       var text = ctx.content;
+       if (RegExp(".ping", "i").exec(text)) {
+           let t0 = performance.now();
+           let t1 = performance.now();
+           let diff = ((t1 - t0) / 1000).toLocaleString('id-ID', { maximumFractionDigits: 3 });
+           let message = `Pong!\nIn <code>${diff}</code> seconds.`
+           return ctx.reply(message)
+       }
+       if (RegExp('.help',"i").exec(text)){
+           return ctx.reply('Available Command\n .ping .help .google .lirik')
+       }                                                                                                 
+       if (RegExp(".lirik", 'i').exec(text)) {                                                                               
+           var reqy = await axios.get(`https://lyrics-api.xlaaf.repl.co/search?q=${text.replace(/([.*kbbi ])/ig,"")}`)
+           var message = 'Ditemukan: '+reqy.data.data
+           return ctx.reply(message)
+       }
+       if (RegExp(".google ", "i").exec(text)) {
            var data = await axios.get(`https://google-api.xlaaf.repl.co/search?q=${text.replace(/([.*google ])/ig,"")}`)
            var ok = data.data.data
            var hai = ok[Math.floor(Math.random() * (ok.length))] 
@@ -32,10 +33,10 @@ client.on('messageCreate', async function(ctx) {
            var desk = hai.desk
            var pesan = 'Ditemukan : '+text+'\n'+judul+'\nUrl: '+link+'\nDeskripsi: '+desk
             return ctx.reply(pesan);
+       }
+  } catch (e) {
+        console.log(e)
     }
- } catch (e) {
-     console.log(e)
- }
 
 });
 
